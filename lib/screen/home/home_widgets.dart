2a7d150dc3f1340800799acb4d945a3cbba1bc44
jinhan <jinhan38@naver.com>
2025-08-29 15:22:12 +0900

    shortCut 위젯 완료

--- lib/screen/home/home_widgets.dart
+++ lib/screen/home/home_widgets.dart
@@ -8,9 +8,24 @@ class HomeWidgets {
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
       children: List.generate(iconList.length, (index) {
-        return ElevatedButton(
-          onPressed: () {},
-          child: Image.asset(iconList[index], fit: BoxFit.cover),
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                maximumSize: Size(70, 70),
+                padding: EdgeInsets.all(16),
+                elevation: 0,
+                backgroundColor: Colors.yellow,
+                foregroundColor: Colors.purple,
+              ),
+              child: Image.asset(iconList[index], fit: BoxFit.cover),
+            ),
+          ],
         );
       }),
     );
